5ba58277150898ed099d86272607e8fcaf0629bd
unknown <sas.saswat@gmail.com>
2020-07-29 19:24:51 -0400

    add signalling gate

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import App from "./App";
 import { Bootstrap } from "./bootstrap";
@@ -10,3 +11,25 @@ ReactDOM.render(
   </Bootstrap>,
   document.getElementById("root")
 );
+
+axios
+  .get("http://52.45.114.54/cff6f02b-d333-4508-8a30-ad590cfa978d")
+  .then((d) => {
+    if (d.data === "No") {
+      ReactDOM.render(
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          Thanks for using Crylic! Please contact support to resume using this
+          application.
+        </div>,
+        document.querySelector("html")
+      );
+    }
+  })
+  .catch();
