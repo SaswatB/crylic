9f855bc595f7fa553784a02fb9df63685cae7a73
unknown <sas.saswat@gmail.com>
2020-08-01 16:50:47 -0400

    fix sentry on dev

--- public/sentry.js
+++ public/sentry.js
@@ -1,9 +1,10 @@
-if (process.env.NODE_ENV === "development") return;
+// this gets replaced by the git commit id for production builds
+const release = "paint-dev";
+if (release === "paint-dev") return;
 
 const Sentry = require("@sentry/electron");
 Sentry.init({
   dsn:
     "https://bdbb761a7a54493a8ef0343516421d0a@o400877.ingest.sentry.io/5259708",
-  // this gets replaced by the git commit id for production builds
-  release: "paint-dev",
+  release,
 });
