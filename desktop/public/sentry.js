f8972456c1807c34f9a7f0586b46af50f75d60f4
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-01-20 02:18:58 -0500

    fix sentry crash on package install

--- desktop/public/sentry.js
+++ desktop/public/sentry.js
@@ -2,7 +2,10 @@
 const release = "paint-dev";
 if (release === "paint-dev") return;
 
-const Sentry = require("@sentry/electron");
+const Sentry =
+  process.type === undefined
+    ? require("@sentry/node")
+    : require("@sentry/electron");
 Sentry.init({
   dsn:
     "https://bdbb761a7a54493a8ef0343516421d0a@o400877.ingest.sentry.io/5259708",
