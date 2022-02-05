349b998ece5a5f88377022491e6f2169290b5933
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-02-05 13:54:54 -0500

    avoid ast recalculations on every editor cursor change

--- desktop/src/components/SideBar/CodeEditorPane/CodeEditor.tsx
+++ desktop/src/components/SideBar/CodeEditorPane/CodeEditor.tsx
@@ -8,14 +8,99 @@ import { useDebounce } from "synergy/src/hooks/useDebounce";
 import { useObservable } from "synergy/src/hooks/useObservable";
 import { useUpdatingRef } from "synergy/src/hooks/useUpdatingRef";
 import { JSXActionProvider } from "synergy/src/lib/ast/providers/JSXActionProvider";
+import { ASTType } from "synergy/src/lib/ast/types";
 import { editorOpenLocation, editorResize } from "synergy/src/lib/events";
 import { CodeEntry } from "synergy/src/lib/project/CodeEntry";
 import { Project } from "synergy/src/lib/project/Project";
-import { isDefined, ltTakeNext, takeNext } from "synergy/src/lib/utils";
+import { isDefined, ltTakeNext } from "synergy/src/lib/utils";
 
 import { setupLanguageService } from "../../../utils/moncao-helpers";
 
 const fs = __non_webpack_require__("fs") as typeof import("fs");
+
+function refreshDecorations(
+  project: Project,
+  { editor }: MonacoEditor,
+  codeEntry: CodeEntry,
+  ast: ASTType,
+  selectedElementId: string | undefined,
+  activeHighlightsRef: React.MutableRefObject<string[]>,
+  activeActionsRef: React.MutableRefObject<string[]>,
+  onCodeChange: (codeId: string, newCode: string) => void
+) {
+  let decorations: monaco.editor.IModelDeltaDecoration[] = [];
+  if (
+    selectedElementId &&
+    project.primaryElementEditor.getCodeIdFromLookupId(selectedElementId) ===
+      codeEntry.id
+  ) {
+    try {
+      decorations = project.primaryElementEditor.getEditorDecorationsForElement(
+        { ast, codeEntry },
+        selectedElementId
+      );
+    } catch (err) {
+      console.log(err);
+    }
+
+    if (decorations.length > 0) {
+      // center to the first decoration if the editor doesn't currently have focus
+      if (!editor?.hasTextFocus()) {
+        editor?.revealPositionInCenter({
+          lineNumber: decorations[0]!.range.startLineNumber,
+          column: decorations[0]!.range.startColumn,
+        });
+      }
+    }
+  }
+
+  // update the highlight decorations on the editor
+  const oldDecorations = activeHighlightsRef.current;
+  if (oldDecorations.length !== 0 || decorations.length !== 0) {
+    console.log("refreshing monaco decorations", codeEntry.id);
+    activeHighlightsRef.current =
+      editor?.deltaDecorations(oldDecorations, decorations) || [];
+  }
+
+  // add in editor actions
+  editor?.changeViewZones((changeAccessor) => {
+    activeActionsRef.current.forEach((viewZoneId) =>
+      changeAccessor.removeZone(viewZoneId)
+    );
+    activeActionsRef.current = [];
+    if (!codeEntry.isScriptEntry) return;
+
+    try {
+      const actions = new JSXActionProvider().getEditorActions(codeEntry);
+
+      actions.forEach((action) => {
+        const linkNode = document.createElement("a");
+        linkNode.className =
+          "absolute z-20 cursor-pointer opacity-75 text-xs hover:opacity-100 hover:underline";
+        linkNode.style.marginLeft = `${action.column * 7.7}px`;
+        linkNode.text = "Move style to style sheet";
+        linkNode.onclick = () => {
+          const changes = new JSXActionProvider().runEditorActionOnAST(
+            action,
+            project
+          );
+          changes.forEach((c) => onCodeChange(c.id, c.code));
+        };
+        const domNode = document.createElement("div");
+        domNode.appendChild(linkNode);
+
+        const viewZoneId = changeAccessor.addZone({
+          afterLineNumber: action.line - 1,
+          heightInLines: 1,
+          domNode: domNode,
+        });
+        activeActionsRef.current.push(viewZoneId);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
 
 interface Props {
   project: Project;
@@ -36,6 +121,7 @@ export const CodeEditor: FunctionComponent<Props> = ({
 }) => {
   const editorRef = useRef<MonacoEditor>(null);
   const code = useObservable(codeEntry.code$);
+  const ast = useObservable(codeEntry.ast$);
   const [localValue, setLocalValue] = useBoundState(code);
   const localValueRef = useUpdatingRef(localValue);
   const [debouncedLocalValue] = useDebounce(localValue, 1000);
@@ -53,91 +139,33 @@ export const CodeEditor: FunctionComponent<Props> = ({
   }, [isActiveEditor]);
 
   // highlight the selected element in the editor
-  const activeHighlights = useRef<string[]>([]);
-  const activeActions = useRef<string[]>([]);
+  const activeHighlightsRef = useRef<string[]>([]);
+  const activeActionsRef = useRef<string[]>([]);
   useEffect(() => {
-    requestAnimationFrame(() => {
-      setTimeout(async () => {
-        let decorations: monaco.editor.IModelDeltaDecoration[] = [];
-        if (
-          selectedElementId &&
-          project.primaryElementEditor.getCodeIdFromLookupId(
-            selectedElementId
-          ) === codeEntry.id
-        ) {
-          try {
-            decorations = project.primaryElementEditor.getEditorDecorationsForElement(
-              { ast: (await ltTakeNext(codeEntry.ast$))!, codeEntry },
-              selectedElementId
-            );
-          } catch (err) {
-            console.log(err);
-          }
+    const handle = requestAnimationFrame(
+      () =>
+        editorRef.current &&
+        refreshDecorations(
+          project,
+          editorRef.current,
+          codeEntry,
+          ast,
+          selectedElementId,
+          activeHighlightsRef,
+          activeActionsRef,
+          onCodeChange
+        )
+    );
 
-          if (decorations.length > 0) {
-            // center to the first decoration if the editor doesn't currently have focus
-            if (!editorRef.current?.editor?.hasTextFocus()) {
-              editorRef.current?.editor?.revealPositionInCenter({
-                lineNumber: decorations[0]!.range.startLineNumber,
-                column: decorations[0]!.range.startColumn,
-              });
-            }
-          }
-        }
-
-        // update the highlight decorations on the editor
-        const oldDecorations = activeHighlights.current;
-        if (oldDecorations.length !== 0 || decorations.length !== 0) {
-          console.log("refreshing monaco decorations", codeEntry.id);
-          activeHighlights.current =
-            editorRef.current?.editor?.deltaDecorations(
-              oldDecorations,
-              decorations
-            ) || [];
-        }
-
-        // add in editor actions
-        editorRef.current?.editor?.changeViewZones((changeAccessor) => {
-          activeActions.current.forEach((viewZoneId) =>
-            changeAccessor.removeZone(viewZoneId)
-          );
-          activeActions.current = [];
-          if (!codeEntry.isScriptEntry) return;
-
-          try {
-            const actions = new JSXActionProvider().getEditorActions(codeEntry);
-
-            actions.forEach((action) => {
-              const linkNode = document.createElement("a");
-              linkNode.className =
-                "absolute z-20 cursor-pointer opacity-75 text-xs hover:opacity-100 hover:underline";
-              linkNode.style.marginLeft = `${action.column * 7.7}px`;
-              linkNode.text = "Move style to style sheet";
-              linkNode.onclick = () => {
-                const changes = new JSXActionProvider().runEditorActionOnAST(
-                  action,
-                  project
-                );
-                changes.forEach((c) => onCodeChange(c.id, c.code));
-              };
-              const domNode = document.createElement("div");
-              domNode.appendChild(linkNode);
-
-              const viewZoneId = changeAccessor.addZone({
-                afterLineNumber: action.line - 1,
-                heightInLines: 1,
-                domNode: domNode,
-              });
-              activeActions.current.push(viewZoneId);
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        });
-      }, 150);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [codeEntry, debouncedLocalValue, selectedElementId]);
+    return () => cancelAnimationFrame(handle);
+  }, [
+    ast,
+    codeEntry,
+    debouncedLocalValue,
+    onCodeChange,
+    project,
+    selectedElementId,
+  ]);
 
   // try to select the element the editor cursor is at
   useEffect(() => {
