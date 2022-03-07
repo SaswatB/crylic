ffd3153e3fe5c164f67b1e1376730433a04835c0
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-03-06 19:14:19 -0500

    create custom outline tree component

--- synergy/src/components/SideBar/OutlinePaneEntry.tsx
+++ synergy/src/components/SideBar/OutlinePaneEntry.tsx
@@ -5,11 +5,6 @@ import React, {
   useRef,
   useState,
 } from "react";
-import { faExpandAlt, faSync } from "@fortawesome/free-solid-svg-icons";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import TreeItem from "@material-ui/lab/TreeItem";
-import TreeView from "@material-ui/lab/TreeView";
 
 import { useObservable } from "../../hooks/useObservable";
 import { useObservableCallback } from "../../hooks/useObservableCallback";
@@ -24,52 +19,63 @@ import { useProject } from "../../services/ProjectService";
 import { SelectService } from "../../services/SelectService";
 import { OutlineElement, OutlineElementType } from "../../types/paint";
 import { SelectedElement } from "../../types/selected-element";
-import { IconButton } from "../IconButton";
+import { OutlineTree } from "./OutlineTree";
 
-interface RenderTreeContext {
-  onRefresh: () => void;
-  onLabelClick: (node: OutlineElement) => void;
-  onExpandAllNodes: () => void;
+function useOutline(renderEntry: RenderEntry) {
+  const project = useProject();
+  const [outline, setOutline] = useState<OutlineElement>();
+
+  const viewContext = useObservable(renderEntry.viewContext$);
+  const reactMetadata = useObservable(renderEntry.reactMetadata$);
+  const refreshOutline = useCallback(async () => {
+    // todo debounce
+    const rootElement = viewContext?.getRootElement();
+    if (!rootElement) return;
+    console.log("reloading outline");
+
+    setOutline(
+      (await buildOutline(
+        project,
+        renderEntry,
+        rootElement,
+        reactMetadata?.fiberComponentRoot
+      )) || []
+    );
+  }, [project, reactMetadata, renderEntry, viewContext]);
+
+  useEffect(() => void refreshOutline(), [
+    renderEntry,
+    reactMetadata,
+    refreshOutline,
+  ]);
+
+  // recalculate the component view outline when the component view compiles, reloads, or changes its route
+  useObservableCallback(renderEntry.viewReloaded$, refreshOutline);
+  useObservableCallback(renderEntry.domChanged$, refreshOutline);
+  useObservableCallback(
+    renderEntry.compileStatus$,
+    (c) => c === RenderEntryCompileStatus.COMPILED && refreshOutline()
+  );
+
+  const treeNodeIdMap = useMemo(() => {
+    const map = new Map<string, OutlineElement>();
+    function recurse(node: OutlineElement) {
+      // it's very important not to render duplicate node ids https://github.com/mui-org/material-ui/issues/20832
+      while (map.has(node.id)) {
+        console.error("duplicate node id", node.id, map, outline);
+        node.id = node.id + "+";
+      }
+      map.set(node.id, node);
+
+      node.children.forEach((c) => recurse(c));
+    }
+    if (outline) recurse(outline);
+
+    return map;
+  }, [outline]);
+
+  return { outline, treeNodeIdMap, refreshOutline };
 }
-
-const renderTree = (context: RenderTreeContext, node: OutlineElement) => {
-  const treeLabel = (
-    <div className="flex">
-      {node.tag}
-      <div className="flex-1" />
-      {node.type === OutlineElementType.Frame && (
-        <>
-          <IconButton
-            title="Expand Outline"
-            className="mr-2"
-            icon={faExpandAlt}
-            onClick={context.onExpandAllNodes}
-          />
-          {/* todo: use a better icon as this icon implies the frame will be refreshed */}
-          <IconButton
-            title="Refresh Outline"
-            icon={faSync}
-            onClick={context.onRefresh}
-          />
-        </>
-      )}
-    </div>
-  );
-
-  return (
-    <TreeItem
-      key={node.id}
-      nodeId={node.id}
-      label={treeLabel}
-      onLabelClick={(e) => {
-        e.preventDefault();
-        context.onLabelClick(node);
-      }}
-    >
-      {node.children.map((c) => renderTree(context, c))}
-    </TreeItem>
-  );
-};
 
 function useExpandedNodes({
   renderEntry,
@@ -159,83 +165,11 @@ interface Props {
 }
 
 export function OutlinePaneEntry({ renderEntry }: Props) {
-  const project = useProject();
   const selectService = useService(SelectService);
-  const [outline, setOutline] = useState<OutlineElement>();
-  const [collapsedNodes, setCollapsedNodes] = useState<string[]>([]);
-
   const selectedElement = useObservable(selectService.selectedElement$);
-  const viewContext = useObservable(renderEntry.viewContext$);
-  const reactMetadata = useObservable(renderEntry.reactMetadata$);
 
-  const calculateOutline = useCallback(async () => {
-    // todo debounce
-    const rootElement = viewContext?.getRootElement();
-    if (!rootElement) return;
-    console.log("reloading outline");
-
-    setOutline(
-      (await buildOutline(
-        project,
-        renderEntry,
-        rootElement,
-        reactMetadata?.fiberComponentRoot
-      )) || []
-    );
-  }, [project, reactMetadata, renderEntry, viewContext]);
-
-  useEffect(() => void calculateOutline(), [
-    renderEntry,
-    reactMetadata,
-    calculateOutline,
-  ]);
-
-  // recalculate the component view outline when the component view compiles, reloads, or changes its route
-  useObservableCallback(renderEntry.viewReloaded$, calculateOutline);
-  useObservableCallback(renderEntry.domChanged$, calculateOutline);
-  useObservableCallback(
-    renderEntry.compileStatus$,
-    (c) => c === RenderEntryCompileStatus.COMPILED && calculateOutline()
-  );
-
-  const treeNodeIdMap = useMemo(() => {
-    const map = new Map<string, OutlineElement>();
-    function recurse(node: OutlineElement) {
-      // it's very important not to render duplicate node ids https://github.com/mui-org/material-ui/issues/20832
-      while (map.has(node.id)) {
-        console.error("duplicate node id", node.id, map, outline);
-        node.id = node.id + "+";
-      }
-      map.set(node.id, node);
-
-      node.children.forEach((c) => recurse(c));
-    }
-    if (outline) recurse(outline);
-
-    return map;
-  }, [outline]);
-
-  const renderedTree = useMemo(() => {
-    if (!outline) return null;
-    return renderTree(
-      {
-        onRefresh() {
-          void calculateOutline();
-        },
-        onLabelClick(node) {
-          if (node.element) {
-            void selectService.selectElement(renderEntry, {
-              htmlElement: node.element,
-            });
-          }
-        },
-        onExpandAllNodes() {
-          setCollapsedNodes([]);
-        },
-      },
-      outline
-    );
-  }, [calculateOutline, outline, renderEntry, selectService]);
+  const { outline, treeNodeIdMap, refreshOutline } = useOutline(renderEntry);
+  const [collapsedNodes, setCollapsedNodes] = useState<string[]>([]);
 
   const { expandedNodes, selectedElementNodeId } = useExpandedNodes({
     renderEntry,
@@ -247,12 +181,11 @@ export function OutlinePaneEntry({ renderEntry }: Props) {
   });
 
   return (
-    <TreeView
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
+    <OutlineTree
+      outline={outline}
       expanded={expandedNodes}
       selected={selectedElementNodeId || ""}
-      onNodeToggle={(e, newExpandedNodes) =>
+      onNodeToggle={(newExpandedNodes) =>
         // only store nodes that have been collapsed
         setCollapsedNodes(
           Array.from(treeNodeIdMap.keys()).filter(
@@ -260,8 +193,15 @@ export function OutlinePaneEntry({ renderEntry }: Props) {
           )
         )
       }
-    >
-      {renderedTree}
-    </TreeView>
+      onExpandAllNodes={() => setCollapsedNodes([])}
+      onRefresh={refreshOutline}
+      onNodeSelected={(node) => {
+        if (node.element) {
+          void selectService.selectElement(renderEntry, {
+            htmlElement: node.element,
+          });
+        }
+      }}
+    />
   );
 }
---
+++ synergy/src/components/SideBar/OutlineTree.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  faChevronDown,
+  faChevronRight,
+  faExpandAlt,
+  faSync,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { OutlineElement, OutlineElementType } from "../../types/paint";
+import { IconButton } from "../IconButton";
+
+interface TreeContext {
+  expanded: string[];
+  selected: string;
+  onRefresh: () => void;
+  onExpandAllNodes: () => void;
+  onNodeToggle: (newExpandedNodes: string[]) => void;
+  onNodeSelected: (node: OutlineElement) => void;
+}
+
+interface OutlineTreeItemProps extends TreeContext {
+  node: OutlineElement;
+}
+
+function OutlineTreeItem({ node, ...context }: OutlineTreeItemProps) {
+  const isExpanded = context.expanded.includes(node.id);
+  const isSelected = node.id === context.selected;
+
+  const renderChildrenToggle = () =>
+    isExpanded ? (
+      <IconButton
+        icon={faChevronDown}
+        className="text-xs"
+        onClick={() =>
+          context.onNodeToggle(context.expanded.filter((id) => id !== node.id))
+        }
+      />
+    ) : (
+      <IconButton
+        icon={faChevronRight}
+        className="text-xs"
+        onClick={() => context.onNodeToggle([...context.expanded, node.id])}
+      />
+    );
+
+  return (
+    <div>
+      <div className="flex">
+        {node.children.length > 0 && renderChildrenToggle()}
+        <div
+          className={`flex flex-1 ml-1 pl-1 bg-gray-500 bg-opacity-0 hover:bg-opacity-50 cursor-pointer ${
+            isSelected && "bg-opacity-25"
+          }`}
+          onClick={() => context.onNodeSelected(node)}
+        >
+          {node.tag}
+          <div className="flex-1" />
+          {node.type === OutlineElementType.Frame && (
+            <>
+              <IconButton
+                title="Expand Outline"
+                className="mr-2"
+                icon={faExpandAlt}
+                onClick={context.onExpandAllNodes}
+              />
+              {/* todo: use a better icon as this icon implies the frame will be refreshed */}
+              <IconButton
+                title="Refresh Outline"
+                icon={faSync}
+                onClick={context.onRefresh}
+              />
+            </>
+          )}
+        </div>
+      </div>
+      {isExpanded ? (
+        <div
+          className="border-l border-gray-700"
+          style={{ marginLeft: 5, paddingLeft: 10 }}
+        >
+          {node.children.map((child) => (
+            <OutlineTreeItem key={child.id} node={child} {...context} />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+interface OutlineTreeProps extends TreeContext {
+  outline: OutlineElement | undefined;
+}
+
+export function OutlineTree({ outline, ...context }: OutlineTreeProps) {
+  if (!outline) return null;
+
+  return <OutlineTreeItem node={outline} {...context} />;
+}
