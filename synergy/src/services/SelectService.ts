768adeb70be4df631c8df44e66da5623ce87909b
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-03-14 13:16:52 -0400

    add select style group log

--- synergy/src/services/SelectService.ts
+++ synergy/src/services/SelectService.ts
@@ -172,6 +172,7 @@ export class SelectService {
   }
 
   public setSelectedStyleGroup(selectedStyleGroup: StyleGroup | undefined) {
+    console.log("changing style group", selectedStyleGroup?.lookupId);
     this.selectedStyleGroup$.next(selectedStyleGroup);
   }
 
