8e27eafacc911f99c4d8ecc6804bbfe3f6860a83
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-03-15 23:06:41 -0400

    fix deps install

--- desktop/public/sentry.js
+++ desktop/public/sentry.js
@@ -2,19 +2,23 @@
 const release = "paint-dev";
 if (release === "paint-dev") return;
 
-const Store = require("electron-store");
-const store = new Store();
+let Sentry;
+let isTrackingDisabled;
 
-const Sentry =
-  process.type === undefined
-    ? require("@sentry/node")
-    : require("@sentry/electron");
+if (process.type !== undefined) {
+  const Store = require("electron-store");
+  const store = new Store();
+  Sentry = require("@sentry/electron");
+  isTrackingDisabled = () => store.get("tracking_disabled") === true;
+} else {
+  // electron-child support
+  Sentry = require("@sentry/node");
+  isTrackingDisabled = () => false; // tracking is disabled through a separate mechanism
+}
+
 Sentry.init({
   dsn:
     "https://bdbb761a7a54493a8ef0343516421d0a@o400877.ingest.sentry.io/5259708",
   release,
-  beforeSend: (e) => {
-    if (store.get("tracking_disabled") === true) return null;
-    return e;
-  },
+  beforeSend: (e) => (isTrackingDisabled() ? null : e),
 });
