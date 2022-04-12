a047693e03c99b80aeefff2edfd46801a514c573
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-04-12 02:20:00 -0400

    reorganize figma code

--- figma/src/index.ts
+++ figma/src/index.ts
@@ -8,18 +8,27 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
+// figma.ui.postMessage()
+
+function recurse(node: SceneNode) {
+  console.log("node", node.type, JSON.stringify(node));
+  if ("children" in node) node.children.forEach(recurse);
+}
+
+figma.currentPage.selection.forEach(recurse);
+
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === "create-rectangles") {
     const nodes: SceneNode[] = [];
     for (let i = 0; i < msg.count; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
