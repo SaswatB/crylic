f4422cc65f59868003981d1d3f50eba406a1df9a
Saswat Bhattacharya <sas.saswat@gmail.com>
2022-12-11 18:55:00 -0500

    remove license check

--- desktop/src/index.tsx
+++ desktop/src/index.tsx
@@ -38,63 +38,26 @@ if (__IS_PRODUCTION__) {
   });
 }
 
-/* eslint-disable simple-import-sort/sort, import/first */
+/* eslint-disable simple-import-sort/imports */
+/* eslint-disable import/first */
 import "reflect-metadata";
 import "./hook.tsx";
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
-
 import { App } from "./App";
 import { Bootstrap } from "synergy/src/bootstrap";
 import { EulaForm } from "synergy/src/components/Support/EulaForm";
 import { loadWASM } from "onigasm";
 
-loadWASM(require("onigasm/lib/onigasm.wasm").default);
+loadWASM(require("onigasm/lib/onigasm.wasm").default).catch((e) => {
+  console.error("Failed to load onigasm.wasm", e);
+});
 
-axios
-  .get("http://52.45.114.54/e5be07bf-fb78-4ecc-b8a2-7fc253bb3c7b")
-  .then((d) => {
-    if (d?.data === "Yes") {
-      ReactDOM.render(
-        <Bootstrap>
-          <EulaForm>
-            <App />
-          </EulaForm>
-        </Bootstrap>,
-        document.getElementById("root")
-      );
-    } else {
-      ReactDOM.render(
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-          }}
-        >
-          Thanks for using Crylic! Please contact support to resume using this
-          application.
-        </div>,
-        document.querySelector("html")
-      );
-    }
-  })
-  .catch((e) => {
-    console.log(e);
-    ReactDOM.render(
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-        }}
-      >
-        There was a problem checking the license server, please check your
-        internet connection and try again.
-      </div>,
-      document.querySelector("html")
-    );
-  });
+ReactDOM.render(
+  <Bootstrap>
+    <EulaForm>
+      <App />
+    </EulaForm>
+  </Bootstrap>,
+  document.getElementById("root")
+);
